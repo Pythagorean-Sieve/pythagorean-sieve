a7970270f0ba50dea103f0798a86947ea9fff423
amato-roberto-py <amato.roberto.py@gmail.com>
2026-01-12 12:36:28 +0100

    Update basic_usage.py

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,5 +1,6 @@
+```python
+from pythagorean_sieve import primes_up_to, nth_prime
+
 if __name__ == "__main__":
-    plist = primes_up_to_r_segmented(100, segment_size=100_000)
-    print(plist)
-    print("count =", len(plist))
-    print("734th prime =", nth_prime_r(734))
+    print(primes_up_to(100))
+    print("734th prime =", nth_prime(734))
